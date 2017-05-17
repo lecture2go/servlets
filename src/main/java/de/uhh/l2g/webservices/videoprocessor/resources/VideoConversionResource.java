df3ef021904fb857e5544b9d926667ceaca4ce2a
mttsh <matthias.h@gmx.net>
2017-05-17 10:11:08 +0200

    Allow PUT requests for JSON and URL-Encoded-Forms
    The URL-Encoded-Form is sent by the Opencast http-notify workflow
    handler

--- src/main/java/de/uhh/l2g/webservices/videoprocessor/resources/VideoConversionResource.java
+++ src/main/java/de/uhh/l2g/webservices/videoprocessor/resources/VideoConversionResource.java
@@ -2,9 +2,13 @@ package de.uhh.l2g.webservices.videoprocessor.resources;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -24,11 +28,24 @@ public class VideoConversionResource {
 		return GenericDao.getInstance().get(VideoConversion.class, id);
 	}
 	
+
 	@PUT
-	@Consumes(MediaType.APPLICATION_JSON)
+	@Consumes({MediaType.APPLICATION_JSON})
 	public Response putVideoConversion() {
+		System.out.println("it is a json");
+
 		return null;
 	}
+	
+	@PUT
+	@Consumes({MediaType.APPLICATION_FORM_URLENCODED})
+	public Response putVideoConversionFormData(@FormParam("message") String vc) {
+		System.out.println("it is a form data");
+
+		return null;
+	}
+	
+	
 	
 	@DELETE
 	@Consumes(MediaType.APPLICATION_JSON)
