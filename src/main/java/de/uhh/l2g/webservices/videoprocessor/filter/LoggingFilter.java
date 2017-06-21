3083c6fbaf79c6fb39d1661b55149db53a403d83
mttsh <matthias.h@gmx.net>
2017-06-21 16:33:31 +0200

    Fix logging of request entities
    the inputstream of the entity is now buffered instead of being consumed
    in the filter (and so weren't able to arrive at the jersey deserializer)

--- src/main/java/de/uhh/l2g/webservices/videoprocessor/filter/LoggingFilter.java
+++ src/main/java/de/uhh/l2g/webservices/videoprocessor/filter/LoggingFilter.java
@@ -1,6 +1,9 @@
 package de.uhh.l2g.webservices.videoprocessor.filter;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 import javax.ws.rs.NameBinding;
 import javax.ws.rs.container.ContainerRequestContext;
@@ -10,6 +13,9 @@ import javax.ws.rs.ext.Provider;
 import org.apache.commons.io.IOUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.glassfish.jersey.message.internal.ReaderWriter;
+import org.glassfish.jersey.server.ContainerException;
+import org.glassfish.jersey.server.ContainerRequest;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -27,14 +33,26 @@ public class LoggingFilter implements ContainerRequestFilter {
 	
 	private static final Logger logger = LogManager.getLogger(LoggingFilter.class);
 
-	
 	@Override
 	public void filter(ContainerRequestContext requestContext) throws IOException {
+		//ContainerRequest request = (ContainerRequest) requestContext;
 		String method = requestContext.getMethod();
 		String path = requestContext.getUriInfo().getPath();
-		String entity = IOUtils.toString(requestContext.getEntityStream(), StandardCharsets.UTF_8.name());
+		logger.info("HTTP Request: {} {}",method, path);
 		
-		logger.info("HTTP Request: {} {} | Entity: {}", method, path, entity);
+		if (requestContext instanceof ContainerRequest) {
+            ContainerRequest request = (ContainerRequest) requestContext;
+
+            if (requestContext.hasEntity()) {
+                request.bufferEntity();
+                String entity = request.readEntity(String.class);
+        		logger.info("Entity: {}", entity);
+
+            }
+        }
+		
+		
+		//String entity = IOUtils.toString(requestContext.getEntityStream(), StandardCharsets.UTF_8.name());
 		
 	}
 	
