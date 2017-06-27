ad22999ca4940d2d4f13c335e86ba909902e2722
mttsh <matthias.h@gmx.net>
2017-06-27 12:15:45 +0200

    Add Cors filter

---
+++ src/main/java/de/uhh/l2g/webservices/videoprocessor/filter/CORSResponseFilter.java
@@ -0,0 +1,22 @@
+package de.uhh.l2g.webservices.videoprocessor.filter;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
+import java.io.IOException;
+
+/**
+ * This class is necessary to allow Cross-Origin Resource Sharing
+ */
+@Provider
+public class CORSResponseFilter implements ContainerResponseFilter {
+
+    @Override
+    public void filter(ContainerRequestContext request, ContainerResponseContext response) throws IOException {
+        response.getHeaders().add("Access-Control-Allow-Origin", "*");
+        response.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+        response.getHeaders().add("Access-Control-Allow-Headers", "Content-Type, accept, authorization");
+        response.getHeaders().add("Access-Control-Expose-Headers", "Location");
+    }
+}
