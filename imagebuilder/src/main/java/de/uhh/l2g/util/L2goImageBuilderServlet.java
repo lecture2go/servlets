93a9f3caf7619aee92b4a0a8fb1e03bb934fa185
mttsh <matthias.h@gmx.net>
2018-02-21 16:11:37 +0100

    Imagebuilder initial commit
    This image builder for creating metadata images for l2go videos was
    created a few years ago, but was missing from the git repository until
    now.
    
    It was refactored to use maven instead of ant.

---
+++ imagebuilder/src/main/java/de/uhh/l2g/util/L2goImageBuilderServlet.java
@@ -0,0 +1,97 @@
+package de.uhh.l2g.util;
+
+import java.awt.FontFormatException;
+import java.awt.RenderingHints;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+import javax.imageio.ImageIO;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class L2goImageBuilderServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public L2goImageBuilderServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("image/png");
+		
+		// reads the request parameters or if not given set them to an empty string
+		String author = request.getParameter("author")!=null ? request.getParameter("author") : "";
+		String institution = request.getParameter("institution")!=null ? request.getParameter("institution") : "";
+		String title = request.getParameter("title")!=null ? request.getParameter("title") : "";
+		String series = request.getParameter("series")!=null ? request.getParameter("series") : "";
+		String date = request.getParameter("date")!=null ? request.getParameter("date") : "";		
+		String type = request.getParameter("type")!=null ? request.getParameter("type") : "";
+		
+		if (type.equals("speakerslides")) {
+			SpeakerSlidesL2goImageBuilder imageBuilder = new SpeakerSlidesL2goImageBuilder(author, institution, title, series, date);
+			InputStream backgroundimageStream = getServletContext().getResourceAsStream("/WEB-INF/generic_speaker_slides.png");
+			imageBuilder.setBackgroundimageStream(backgroundimageStream);
+
+			imageBuilder.setFontSize(30.0f);
+			initializeFonts(imageBuilder);
+			
+			// builds a large image with the given data
+			BufferedImage image = imageBuilder.buildImage();
+		
+			// scales the image down to the needed size
+			BufferedImage scaledImage = imageBuilder.scaleImage(image, 316, 240, RenderingHints.VALUE_INTERPOLATION_BICUBIC, true);
+			
+			// finally write the image to the browser as PNG file
+			ImageIO.write(scaledImage, "png", response.getOutputStream());
+		} else if (type.equals("speakeronly")) {
+			SpeakerOnlyL2goImageBuilder imageBuilder = new SpeakerOnlyL2goImageBuilder(author, institution, title, series, date);
+			InputStream backgroundimageStream = getServletContext().getResourceAsStream("/WEB-INF/generic_speakeronly.png");
+			imageBuilder.setBackgroundimageStream(backgroundimageStream);
+
+			imageBuilder.setFontSize(30.0f);
+			initializeFonts(imageBuilder);
+			
+			// builds a large image with the given data
+			BufferedImage image = imageBuilder.buildImage();
+			
+			// scales the image down to the needed size
+			BufferedImage scaledImage = imageBuilder.scaleImage(image, 720, 75, RenderingHints.VALUE_INTERPOLATION_BICUBIC, true);
+			
+			// finally write the image to the browser as PNG file
+			ImageIO.write(scaledImage, "png", response.getOutputStream());
+		} 
+	}
+	
+	/**
+	 * Reads the font files for the different font types (bold italic not implemented)
+	 * @param imageBuilder the L2goImageBuilder for which the fonts are set
+	 * @throws FontFormatExceptions
+	 */
+	public void initializeFonts(L2goImageBuilder imageBuilder) {
+		try {
+			// the regular font type
+			InputStream fontRegularStream = getServletContext().getResourceAsStream("/WEB-INF/TheSansUHH_Regular.ttf");
+			imageBuilder.setFontRegular(fontRegularStream);
+			
+			// the bold font type
+			InputStream fontBoldStream = getServletContext().getResourceAsStream("/WEB-INF/TheSansUHH_Bold.ttf");
+			imageBuilder.setFontBold(fontBoldStream);
+			
+			// the italic font type
+			InputStream fontItalicStream = getServletContext().getResourceAsStream("/WEB-INF/TheSansUHH_Regular_Italic.ttf");
+			imageBuilder.setFontItalic(fontItalicStream);			
+		} catch(IOException e) {
+			e.printStackTrace();
+		} catch(FontFormatException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ imagebuilder/src/main/java/de/uhh/l2g/util/SpeakerOnlyL2goImageBuilder.java
@@ -0,0 +1,108 @@
+package de.uhh.l2g.util;
+
+import java.awt.RenderingHints;
+import java.awt.image.BufferedImage;
+
+/**
+ * Builds a Lecture2Go (lecture2go.uni-hamburg.de)-specific metadata-image to be encoded with a video
+ * This is the "speaker-slides" version
+ */
+public class SpeakerOnlyL2goImageBuilder extends L2goImageBuilder {
+	
+	/**
+	 * The constructor method, sets a default backgroundimage-stream and sizing values
+	 * @param author the metadata (author), which will be written on the image
+	 * @param institution the metadata (institution), which will be written on the image
+	 * @param title the metadata (title), which will be written on the image
+	 * @param series the metadata (series), which will be written on the image
+	 * @param date the metadata (date), which will be written on the image
+	 */
+	public SpeakerOnlyL2goImageBuilder(String author, String institution, String title, String series, String date) {
+		super(author, institution, title, series, date);
+	
+		// the image width (in pixels) is used to determine the maximum text length per line
+		this.imageWidth  = 1440;
+		
+		// the distance from the image-border to the start of text in pixels
+		this.offsetLeft = 330;
+		this.offsetRight = 260;
+	}
+
+	/**
+	 * Builds the image from the given metadata and backgroundimage
+	 * @return the drawn image 
+	 */
+    public BufferedImage buildImage()  {    
+    	// the image is prepared for editing
+    	this.prepareImageCreation();
+    	
+    	int fontSize = (int) (this.getfontSize());
+		this.lineHeight = fontSize + fontSize/10;
+
+        // we declare some position variables
+        int authorYPosition;
+		int institutionYPosition;
+		int institutionXPosition;
+        int titleYPosition;
+        int seriesYPosition;
+    	
+        // series and date are handled as one
+    	String seriesAndDate = "[ " + this.series + " / " + this.date + " ]"; 
+   	    
+    	System.out.println(this.g.getRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING));
+    	System.out.println(this.g.getRenderingHint(RenderingHints.KEY_RENDERING));
+    	
+    	// the author position is fixed
+    	authorYPosition = 32;
+	    this.g.setFont(this.fontBold);
+	    int widthAuthor = this.g.getFontMetrics().stringWidth(this.author);
+	    this.drawString(g, this.author, this.offsetLeft, authorYPosition, this.maxTextWidth, 1);
+
+	    // used lines for a drawn string
+	    int usedLines;
+	    // maximum lines to draw for a string
+	    int maxLines;
+	    
+	    // a prefix to fill the gap between author and institution if necessary
+	    String institutionPrefix = ", ";
+
+	    this.g.setFont(this.fontItalic);
+	    
+	    int widthInstitution = this.g.getFontMetrics().stringWidth(this.institution);
+	    // if the author and institution fits in one line, do it (separated by prefix)
+        if (widthAuthor + widthInstitution <= this.maxTextWidth) {
+            institutionYPosition = 32;
+            institutionXPosition = this.offsetLeft + widthAuthor;
+            usedLines = 1;
+        } else {
+        	institutionPrefix = "";
+        	institutionYPosition = 64;
+            institutionXPosition = this.offsetLeft;
+            usedLines = 2;
+        }
+	   	this.drawString(this.g, institutionPrefix + this.institution, institutionXPosition, institutionYPosition, this.maxTextWidth, 1);
+    	// the title position
+	    if (usedLines <= 1) {
+	    	titleYPosition = 72;
+	    	maxLines = 2;
+	    } else {
+	    	titleYPosition = 100;
+	    	maxLines = 1;
+	    }
+	   	
+	  	this.g.setFont(this.fontRegular);
+	  	usedLines = this.drawString(this.g, this.title, this.offsetLeft, titleYPosition, this.maxTextWidth, maxLines);
+	  	
+	    // the series/date position is fixed
+  		seriesYPosition = 138;
+	  	this.g.setFont(this.fontItalic);
+
+	   	this.drawString(this.g, seriesAndDate, this.offsetLeft, seriesYPosition, this.maxTextWidth, 1);
+	   	
+	    // clean up
+	    this.g.dispose();
+    	
+    	return image;
+    }
+
+}
